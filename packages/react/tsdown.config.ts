8f4e48c01611dd3b2f08fc41570bc62f00af4c28
Marmotize <hello@marmotize.dev>
2025-10-05 01:38:30 +0200

    docs: remove redundant basic react readme

--- packages/react/tsdown.config.ts
+++ packages/react/tsdown.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
+  external: ['react', '@trace-viz/core'],
   format: ['esm'],
   sourcemap: true,
   treeshake: true,
